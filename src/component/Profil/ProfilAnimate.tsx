255668268fca96c89510ea5d7f103709b57e0772
Dimas Indra Jaya <dimaszxc101@gmail.com>
2023-10-10 13:17:25 +0700

    aniamte profil

---
+++ src/component/Profil/ProfilAnimate.tsx
@@ -0,0 +1,28 @@
+import '../../style/Navbar/profilAnimate.scss';
+
+function ProfilAnimate() {
+  return (
+    <>
+      <div className="container">
+        <div className="container-profil">
+          <div className=" profil">
+            <img src="./src/assets/img/indra.jpg" alt="profil" />
+          </div>
+        </div>
+        <div className="text-profil">
+          <p className="description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            inventore sequi totam qui doloremque temporibus tempore cumque autem
+            blanditiis sit nihil, vitae consequuntur. Soluta tempora corporis
+            sed, deserunt eveniet nam.
+          </p>
+
+          <p className="name">Dimas Indra Jaya</p>
+          <p className="job">Full Stack Developer</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProfilAnimate;
--- src/component/Profil/ProfilWelcome.tsx
+++ src/component/Profil/ProfilWelcome.tsx
@@ -1,7 +1,24 @@
+import ProfilAnimate from './ProfilAnimate';
+
 function ProfilWelcome() {
   return (
     <>
-      <div>Hello World</div>
+      <div className="w-full h-[100vh] flex flex-col justify-center px-40">
+        <div className="h-auto w-4/6 bg-[#272932]">
+          <div className="flex w-full h-full relative">
+            <div className="w-[50%] m-10">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam,
+              qui aliquid magni iure quos placeat cum reprehenderit ad cumque
+              ipsa deserunt. Minus voluptas necessitatibus ut hic quae possimus
+              ullam amet commodi dolor, itaque vel iusto rem voluptatem neque
+              repellat nemo sapiente, est cumque iste sunt blanditiis. Eligendi
+              voluptatem vel quam?
+            </div>
+            <ProfilAnimate></ProfilAnimate>
+          </div>
+        </div>
+        <div className="h-28 w-full"></div>
+      </div>
     </>
   );
 }
