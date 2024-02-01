608a442e05b8f29947ab7039bcc4966e53decb8a
Dimas Indra Jaya <dimaszxc101@gmail.com>
2024-02-01 15:16:07 +0700

    sertif - acievement

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import NavbarLeft from './component/Navbar/NavbarLeft.tsx';
 import NavbarBottom from './component/Navbar/NavbarBottom.tsx';
 import HomeWelcome from './component/Home/HomeWelcome.tsx';
 import AboutProfil from './component/About/AboutProfil.tsx';
-import Sertificate from './component/Sertificate/Sertificate.tsx';
+import Sertificate from './component/Achievement/Sertificate.tsx';
 
 function App() {
   return (
---
+++ src/component/Achievement/Sertificate.tsx
@@ -0,0 +1,152 @@
+import '../../style/Achievement/sertificate.scss';
+
+function Sertificate() {
+  return (
+    <>
+      <div>
+        <div className="container-section" id="achievement">
+          <div className="heading">
+            <h1>Sertificate</h1>
+          </div>
+          <div className="content-achievement">
+            <div className="achievement-content">
+              <div className="container-achievement">
+                <div className="achievement-img"></div>
+                <div className="achievement-des"></div>
+              </div>
+              <div className="container-choice">
+                <div className="choice-des">
+                  <div className="choice-buttons"></div>
+                </div>
+                <div className="choice">
+                  <div className="choice-isi">
+                    <div className="img-choice"></div>
+                    <div className="des-choice">
+                      <h1>lorem10</h1>
+                      <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Recusandae consequuntur hic facilis, placeat
+                        molestias animi nobis veniam accusantium quibusdam
+                        dignissimos?
+                      </p>
+                    </div>
+                  </div>
+                  <div className="choice-isi">
+                    <div className="img-choice"></div>
+                    <div className="des-choice">
+                      <h1>lorem10</h1>
+                      <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Recusandae consequuntur hic facilis, placeat
+                        molestias animi nobis veniam accusantium quibusdam
+                        dignissimos?
+                      </p>
+                    </div>
+                  </div>
+                  <div className="choice-isi">
+                    <div className="img-choice"></div>
+                    <div className="des-choice">
+                      <h1>lorem10</h1>
+                      <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Recusandae consequuntur hic facilis, placeat
+                        molestias animi nobis veniam accusantium quibusdam
+                        dignissimos?
+                      </p>
+                    </div>
+                  </div>
+                  <div className="choice-isi">
+                    <div className="img-choice"></div>
+                    <div className="des-choice">
+                      <h1>lorem10</h1>
+                      <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Recusandae consequuntur hic facilis, placeat
+                        molestias animi nobis veniam accusantium quibusdam
+                        dignissimos?
+                      </p>
+                    </div>
+                  </div>
+                  <div className="choice-isi">
+                    <div className="img-choice"></div>
+                    <div className="des-choice">
+                      <h1>lorem10</h1>
+                      <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Recusandae consequuntur hic facilis, placeat
+                        molestias animi nobis veniam accusantium quibusdam
+                        dignissimos?
+                      </p>
+                    </div>
+                  </div>
+                  <div className="choice-isi">
+                    <div className="img-choice"></div>
+                    <div className="des-choice">
+                      <h1>lorem10</h1>
+                      <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Recusandae consequuntur hic facilis, placeat
+                        molestias animi nobis veniam accusantium quibusdam
+                        dignissimos?
+                      </p>
+                    </div>
+                  </div>
+                  <div className="choice-isi">
+                    <div className="img-choice"></div>
+                    <div className="des-choice">
+                      <h1>lorem10</h1>
+                      <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Recusandae consequuntur hic facilis, placeat
+                        molestias animi nobis veniam accusantium quibusdam
+                        dignissimos?
+                      </p>
+                    </div>
+                  </div>
+                  <div className="choice-isi">
+                    <div className="img-choice"></div>
+                    <div className="des-choice">
+                      <h1>lorem10</h1>
+                      <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Recusandae consequuntur hic facilis, placeat
+                        molestias animi nobis veniam accusantium quibusdam
+                        dignissimos?
+                      </p>
+                    </div>
+                  </div>
+                  <div className="choice-isi">
+                    <div className="img-choice"></div>
+                    <div className="des-choice">
+                      <h1>lorem10</h1>
+                      <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Recusandae consequuntur hic facilis, placeat
+                        molestias animi nobis veniam accusantium quibusdam
+                        dignissimos?
+                      </p>
+                    </div>
+                  </div>
+                  <div className="choice-isi">
+                    <div className="img-choice"></div>
+                    <div className="des-choice">
+                      <h1>lorem10</h1>
+                      <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Recusandae consequuntur hic facilis, placeat
+                        molestias animi nobis veniam accusantium quibusdam
+                        dignissimos?
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Sertificate;
